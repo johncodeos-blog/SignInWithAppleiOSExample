85e5e3b6e378fe69474c12ba2255f314904ab011
John Codeos <johncodeos@gmail.com>
2023-03-17 22:09:08 +0200

    Fix navigation bar color for iOS 15 and above

--- SignInWithAppleExample/LoginViewController.swift
+++ SignInWithAppleExample/LoginViewController.swift
@@ -18,6 +18,15 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Fix navigation bar color in iOS 15 and above
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            appearance.backgroundColor = UIColor(named: "primaryColor")
+            navigationController?.navigationBar.standardAppearance = appearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        }
         // Default 'Sign in with Apple' button
         appleLoginButton()
         
